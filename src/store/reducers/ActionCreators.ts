59b01b2afd859cf4ff5f5a98d52adb4e8490dc22
Daniil <46220468+daangrah@users.noreply.github.com>
2023-09-04 18:53:52 +0300

    working with sagas

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,68 +1,73 @@
-import { type AppDispatch } from '../store'
-import axios from 'axios'
-import { takeEvery, call, all, put } from 'redux-saga/effects'
+import { type AppDispatch } from "../store";
+import axios from "axios";
+import { takeEvery, call, all, put } from "redux-saga/effects";
 import {
   movieFetching,
   movieFetchingSuccess,
   movieVideoSucces,
-  seriesFetchingSuccess
-} from './UserSlice'
+  seriesFetchingSuccess,
+} from "./UserSlice";
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA'
-  }
-}
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA",
+  },
+};
 const fetchTopMoviesFromAPI = async () =>
   await fetch(
-    'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
-    options
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    options,
   )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 
-function * workerGetMovies () {
-  console.log('popal')
-  const data = yield call(fetchTopMoviesFromAPI)
-  console.log(data)
+function* workerGetMovies() {
+  console.log("popal");
+  const data = yield call(fetchTopMoviesFromAPI);
+  console.log(data.json);
   const json = yield call(
     async () =>
-      await new Promise((res) => { res(dispatch(movieFetchingSuccess(data.results))) })
-  )
-  console.log(json)
+      await new Promise((res) => {
+        res(dispatch(movieFetchingSuccess(data.results)));
+      }),
+  );
+  console.log(json);
 }
 
-function * watcherClickSaga () {
-  yield takeEvery(movieFetching, workerGetMovies)
+function* watcherClickSaga() {
+  yield takeEvery(movieFetching, workerGetMovies);
 }
-export function * rootSaga () {
-  yield all([watcherClickSaga()])
+export function* rootSaga() {
+  yield all([watcherClickSaga()]);
 }
 
 export const fetchMovieVideo = (id) => {
   return async (dispatch: AppDispatch) => {
     const options = {
-      method: 'GET',
+      method: "GET",
       url: `https://api.themoviedb.org/3/movie/${id}/videos`,
-      params: { language: 'en-US' },
+      params: { language: "en-US" },
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA'
-      }
-    }
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA",
+      },
+    };
     axios
       .request(options)
       .then(function (response) {
-        dispatch(movieVideoSucces(response.data.results[0].key))
-        console.log(response.data.results[0].key)
+        dispatch(movieVideoSucces(response.data.results[0].key));
+        console.log(response.data.results[0].key);
       })
       .catch(function (error) {
-        console.error(error)
-      })
-  }
-}
+        console.error(error);
+      });
+  };
+};
 
 // export const fetchMovies = () => {
 //   return async (dispatch: AppDispatch) => {
@@ -89,27 +94,27 @@ export const fetchMovieVideo = (id) => {
 // };
 export const fetchSeries = () => {
   return async (dispatch: AppDispatch) => {
-    dispatch(movieFetching())
+    dispatch(movieFetching());
 
     const options = {
-      method: 'GET',
-      url: 'https://api.themoviedb.org/3/tv/top_rated',
-      params: { language: 'en-US' },
+      method: "GET",
+      url: "https://api.themoviedb.org/3/tv/top_rated",
+      params: { language: "en-US" },
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA'
-      }
-    }
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmMyMjA4ZWQ4ODk0OTIwMjY0ZDllNGM3OGZkNDhlYyIsInN1YiI6IjY0OWMzMzJlNzdjMDFmMDBjYTVhYTkxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KtN-jjPloqFEM021pITpc88QdOROqqtVOnTYz3qZbTA",
+      },
+    };
 
     axios
       .request(options)
       .then(function (response) {
-        dispatch(seriesFetchingSuccess(response.data.results))
-        console.log('sosi jopu')
+        dispatch(seriesFetchingSuccess(response.data.results));
+        console.log("sosi jopu");
       })
       .catch(function (error) {
-        console.error(error)
-      })
-  }
-}
+        console.error(error);
+      });
+  };
+};
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,24 @@
 import {
   combineReducers,
   configureStore,
-  getDefaultMiddleware
-} from '@reduxjs/toolkit'
-import movieReducer from './reducers/UserSlice'
-import createSagaMiddleware from 'redux-saga'
-import { rootSaga } from './reducers/ActionCreators'
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import movieReducer from "./reducers/UserSlice";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./reducers/ActionCreators";
 
 const rootReducer = combineReducers({
-  movieReducer
-})
-const sagaMiddleware = createSagaMiddleware()
-export const setupStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => [
-      ...getDefaultMiddleware(),
-      sagaMiddleware
-    ]
-  })
-  sagaMiddleware.run(rootSaga)
-}
+  movieReducer,
+});
+const sagaMiddleware = createSagaMiddleware();
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    sagaMiddleware,
+  ],
+});
+sagaMiddleware.run(rootSaga);
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
